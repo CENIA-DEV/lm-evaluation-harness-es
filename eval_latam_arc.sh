e973167d019fa81c694a41bd409ad03882396ce2
Fuentes149 <marcos.fuentes@uc.cl>
2024-10-17 13:36:56 -0300

    eval paes

--- eval_latam_arc.sh
+++ eval_latam_arc.sh
@@ -1,7 +1,9 @@
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 lm_eval --model hf \
---model_args pretrained=openai-community/gpt2 \
---tasks paes \
---device cuda:6 \
+--model_args pretrained=meta-llama/Llama-3.2-1B  \
+--tasks latam_arc \
+--device cuda:3 \
 --batch_size 32 \
+--num_fewshot 25 \
 --output_path outputs \
 --log_samples
