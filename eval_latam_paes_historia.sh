2ebf7dbcc883fb174f33089695ff96084a250c67
Sebastian Cifuentes Carrasco <30848014+sebcif@users.noreply.github.com>
2024-11-18 15:37:17 -0500

    Refactor old tasks and add PAES with topics (credits to ouhenio) (#4)
    * Update mmlu, hellaswag and arc latam tasks
    
    * Update mmlu, hellaswag and arc eval scripts
    
    * Refactor: change on paes historia
    
    * Fix and add paes_topic benchmark (Made by ouhenio)

--- eval_latam_paes_historia.sh
+++ eval_latam_paes_historia.sh
@@ -1,8 +1,7 @@
 lm_eval --model hf \
 --model_args "pretrained=meta-llama/Llama-3.1-70B-Instruct,parallelize=True" \
---tasks latam_hellaswag \
---device cuda:0 \
---batch_size 32 \
---num_fewshot 10 \
+--tasks paes_historia \
+--batch_size 16 \
+--num_fewshot 0 \
 --output_path outputs \
 --log_samples
